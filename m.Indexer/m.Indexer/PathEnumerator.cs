0aa09108b877bba82bae7817f28b5ce173b213fb
michael <michael@alphero.com>
2015-08-22 17:47:54 +1200

    Init Commit

---
+++ m.Indexer/m.Indexer/PathEnumerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m.Indexer
+{
+    public class PathEnumerator
+    {
+        private readonly string _rootPath;
+
+        public PathEnumerator(String rootPath)
+        {
+            _rootPath = rootPath;
+        }
+
+        public void IndexPath()
+        {
+
+        }
+    }
+}
---
+++ m.Indexer/m.Tests/PathEnumeratorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using m.Indexer;
+using NUnit.Framework;
+
+namespace m.Tests
+{
+    [TestFixture]
+    public class PathEnumeratorTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(()=> new PathEnumerator(null).IndexPath(), Throws.Nothing);
+        }
+    }
+}
